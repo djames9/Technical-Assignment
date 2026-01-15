d96cb1f7268f9584a69d68dd48552b44df4cdc9d
Diego James <die.james.talamas@gmail.com>
2026-01-15 13:08:41 +0200

    models improvement

--- sensor_data/analytics/models.py
+++ sensor_data/analytics/models.py
@@ -20,5 +20,20 @@ class SensorData(models.Model):
 
     def __str__(self):
         return f"{self.sensor_id} @ {self.timestamp}"
+    
+class enterpriseModel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+class location (models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
 
 # Create your models here.
